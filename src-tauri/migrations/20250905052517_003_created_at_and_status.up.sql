da8d383fe508ad65ffdab0487882297816f22bc7
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-09-13 12:26:38 +0700

    refactor(db): change id data types to text as blob wasnt working

--- src-tauri/migrations/20250905052517_003_created_at_and_status.up.sql
+++ src-tauri/migrations/20250905052517_003_created_at_and_status.up.sql
@@ -1,18 +1,18 @@
 create table chats_new(
     created_at integer not null default (cast(unixepoch('now', 'subsecond') * 1000 as integer)),
-    id blob primary key not null,
+    id text primary key not null,
     title text not null
-);
+) without rowid;
 
 create table chat_messages_new (
     created_at integer not null default (cast(unixepoch('now', 'subsecond') * 1000 as integer)),
-    chat_id blob not null,
-    id blob primary key not null,
+    chat_id text not null,
+    id text primary key not null,
     role text not null,
     content text not null,
     status text not null default 'completed' check (status in ('pending', 'completed', 'failed')),
     constraint fk_chat_messages_chats_chat_id foreign key (chat_id) references chats(id) on delete cascade
-);
+) without rowid;
 
 insert into chats_new(created_at, id, title) select created_at, id, title from chats;
 insert into chat_messages_new(created_at, chat_id, id, role, content, status)
